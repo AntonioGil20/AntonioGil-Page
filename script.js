9f76b870eb4d4092694693f0d0db52895e19dae6
AntonioGil20 <juanantoniogillopez2003@gmail.com>
2026-04-11 11:16:50 -0600

    mejoras  y agregue las paginas

--- script.js
+++ script.js
@@ -24,13 +24,6 @@ document.addEventListener('DOMContentLoaded', () => {
             videoCover: "portadas/ToLeaveAndGoCover.mp4",
             portada: "portadas/ToLeaveAndGo.jpg",
             url: "to-leave-and-go.html",
-            descripcion: "Una canción sobre la monotonía. Decidí hacer el video yo mismo usando luces de neón en mi cuarto.",
-            canciones: [
-                { nombre: "01. Love At The Start", archivo: "canciones/Love At The Start.mp3" },
-                { nombre: "02. Growing Wounds", archivo: "canciones/Growing Wounds.mp3"},
-                { nombre: "03. Paralysis Days", archivo: "canciones/Paralysis Days.mp3" },
-                { nombre: "04. OUT FOREVER", archivo: "canciones/OUT FOREVER.mp3" }
-            ]
         },
         {
             id: 3,
@@ -38,10 +31,6 @@ document.addEventListener('DOMContentLoaded', () => {
             videoCover: "portadas/AbrevaderoCover.mp4",
             portada: "portadas/Abrevadero.jpg",
             url: "abrevadero.html",
-            descripcion: "Agrega aquí la historia detrás de este lanzamiento...",
-            canciones: [
-                { nombre: "01. Abrevadero", archivo: "canciones/Abrevadero.mp3" }
-            ]
         },
         {
             id: 4,
@@ -49,12 +38,6 @@ document.addEventListener('DOMContentLoaded', () => {
             videoCover: "portadas/EstoEsUnMusicalCover.mp4",
             portada: "portadas/EstoEsUnMusical.jpg",
             url: "esto-es-un-musical.html",
-            descripcion: "Agrega aquí la historia detrás de este lanzamiento...",
-            canciones: [
-                { nombre: "01. Bajo Los Pies", archivo: "canciones/Bajo Los Pies.mp3" },
-                { nombre: "02. El Anochecer De Quien", archivo: "canciones/El Anochecer De Quien.mp3" },
-                { nombre: "03. Musical Finale (S+3)", archivo: "canciones/Esto Es Un Musical.mp3" }
-            ]
         },
         {
             id: 5,
@@ -62,10 +45,6 @@ document.addEventListener('DOMContentLoaded', () => {
             videoCover: "portadas/HaveYouEverSeenTheRainCover.mp4",
             portada: "portadas/HaveYouEverSeenTheRain.jpg",
             url: "have-you-ever-seen-the-rain.html",
-            descripcion: "Mi propia versión de este clásico. Un tributo con mi estilo de producción.",
-            canciones: [
-                { nombre: "01. Have You Ever Seen The Rain", archivo: "canciones/HaveYouEverSeenTheRain.mp3" }
-            ]
         },
         {
             id: 6,
@@ -94,12 +73,6 @@ document.addEventListener('DOMContentLoaded', () => {
             videoCover: "portadas/ConmociónCover.mp4",
             portada: "portadas/Conmoción.jpg",
             url: "conmocion.html",
-            descripcion: "Agrega aquí la historia detrás de este lanzamiento...",
-            canciones: [
-                { nombre: "01. Conmoción", archivo: "canciones/Conmoción.mp3" },
-                { nombre: "02. Maquiavelo", archivo: "canciones/Maquiavelo.mp3" },
-                { nombre: "03. La Vuelta", archivo: "canciones/La Vuelta.mp3" }
-            ]
         },
         {
             id: 8,
@@ -107,10 +80,6 @@ document.addEventListener('DOMContentLoaded', () => {
             videoCover: "portadas/IcaroCover.mp4",
             portada: "portadas/Icaro.jpg",
             url: "icaro.html",
-            descripcion: "Agrega aquí la historia detrás de este lanzamiento...",
-            canciones: [
-                { nombre: "01. ÍCARO", archivo: "canciones/ÍCARO.mp3" }
-            ]
         },
         {
             id: 9,
@@ -118,10 +87,6 @@ document.addEventListener('DOMContentLoaded', () => {
             videoCover: "portadas/AmorCrimenCover.mp4",
             portada: "portadas/AmorCrimen.jpg",
             url: "amor-crimen.html",
-            descripcion: "Agrega aquí la historia detrás de este lanzamiento...",
-            canciones: [
-                { nombre: "01. amor crimen", archivo: "canciones/amor crimen.mp3" }
-            ]
         },
         {
             id: 10,
@@ -129,10 +94,6 @@ document.addEventListener('DOMContentLoaded', () => {
             videoCover: "portadas/DondeNoLLegaLaSangreCover.mp4",
             portada: "portadas/DondeNoLLegaLaSangre.jpg",
             url: "donde-no-llega-la-sangre.html",
-            descripcion: "Agrega aquí la historia detrás de este lanzamiento...",
-            canciones: [
-                { nombre: "01. Donde No Llega La Sangre", archivo: "canciones/Donde No LLega La Sangre.mp3" }
-            ]
         },
         {
             id: 11,
@@ -140,11 +101,6 @@ document.addEventListener('DOMContentLoaded', () => {
             videoCover: "portadas/PareceQueEsNormalCover.mp4",
             portada: "portadas/PareceQueEsNormal.png",
             url: "parece-que-es-normal.html",
-            descripcion: "Agrega aquí la historia detrás de este lanzamiento...",
-            canciones: [
-                { nombre: "01. 9 De 10 Soy Terco", archivo: "canciones/9 De 10 Soy Terco.mp3" },
-                { nombre: "02. Parece Que Es Normal", archivo: "canciones/parece que es normal.mp3" },
-            ]
         },
         {
             id: 12,
@@ -152,10 +108,6 @@ document.addEventListener('DOMContentLoaded', () => {
             videoCover: "portadas/TodoLoQueOcurreBienEstaEnElExteriorCover.mp4",
             portada: "portadas/TodoLoQueOcurreBienEstaEnElExterior.png",
             url: "todo-lo-que-ocurre.html",
-            descripcion: "Agrega aquí la historia detrás de este lanzamiento...",
-            canciones: [
-                { nombre: "01. Todo Lo Que Ocurre Bien Está En El Exterior", archivo: "canciones/Todo Lo Que Ocurre Bien Está En El Exterior.mp3" }
-            ],
             spotify_embed: '<iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/album/3oyUi5XpOrz2xJCRtKzgPi?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
         }
     ];
